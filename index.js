078c124630dac3d20493cad1053f94da02ad1294
CHankins1021 <122927137+CHankins1021@users.noreply.github.com>
2023-03-04 19:08:34 -1000

    Update index.js

--- index.js
+++ index.js
@@ -15,13 +15,15 @@ app.get("/", function(req, res) {
 app.post("/", function(req, res) {
     
     // takes in the zip from the html form, display in // console. Takes in as string, ex. for zip 02139
-        var zip = String(req.body.zipInput);
-        console.log(req.body.zipInput);
+        var Lat = String(req.body.latInput);
+        console.log(req.body.latInput);
+        var Long = String(req.body.longInput);
+        console.log(req.body.longInput);
     
     //build up the URL for the JSON query, API Key is // secret and needs to be obtained by signup 
         const units = "imperial";
         const apiKey = "21223e71145f84e712ccef41609bcfd9";
-        const url = "https://api.openweathermap.org/data/2.5/weather?q=" + zip +  "&units=" + units + "&APPID=" + apiKey;
+        const url = "https://api.openweathermap.org/data/2.5/weather?lat=" + Lat +"&lon=" + Long  + "&APPID=" + apiKey;
     
     // this gets the data from Open WeatherPI
     https.get(url, function(response){
@@ -33,6 +35,8 @@ app.post("/", function(req, res) {
             const temp = weatherData.main.temp;
             const humidity = weatherData.main.humidity;
             const windSpeed = weatherData.wind.speed;
+          const Cloudiness = 
+            weatherData.clouds.all;
             const city = weatherData.name;
             const weatherDescription = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon;
@@ -44,11 +48,13 @@ app.post("/", function(req, res) {
             res.write("<img src=" + imageURL +">");
             res.write("<h3> The Humidity in " + city + " "  + " is " + humidity + " Percent <h3>");
           res.write("<h4> The Wind Speed in "+ city + " " + " is "+ windSpeed + " Miles Per Hour <h4>");
+          res.write("<h5> The Cloudiness in " + city + " " + " is " + Cloudiness + " Percent  </h5>" ) ; 
             res.send();
             
         });
     });
 })
+
 
 
 //Code will run on 3000 or any available open port
